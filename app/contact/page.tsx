4d10f0aa9ef63a827d74d0553513eea0b7e17f30
aryansingh909 <adarsharyan909@gmail.com>
2026-06-29 22:35:29 +0530

    updated website

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -57,8 +57,7 @@ export default function ContactPage() {
                     <a href="tel:9415660616" className="text-[#0B3D91] hover:text-[#F97316] font-medium text-lg transition-colors">
                       +91 94156 60616
                     </a>
-                  </div>
-                  <div>
+                    <br />
                     <a href="tel:9415660616" className="text-[#0B3D91] hover:text-[#F97316] font-medium text-lg transition-colors">
                       +91 9730469473
                     </a>
